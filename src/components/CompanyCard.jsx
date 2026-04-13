9e452be6ee62974026354d78bb2c65947f62336a
developranadr18vit25 <ranadhruv10@gmail.com>
2026-04-13 18:09:21 +0530

    Working on integration with backend for Register

--- src/components/CompanyCard.jsx
+++ src/components/CompanyCard.jsx
@@ -1,25 +1,22 @@
 import React from 'react'
 
-function CompanyCard() {
+function CompanyCard({companyLink,title}) {
     return (
         <div className="w-72 p-5 border rounded-2xl shadow-sm hover:shadow-md transition">
 
-            {/* Logo */}
             <div className="flex justify-center mb-4">
                 <img
-                    src="https://via.placeholder.com/80"
+                    src={companyLink}
                     alt="company logo"
                     className="w-16 h-16 object-contain"
                 />
             </div>
 
-            {/* Text */}
             <div className="text-center mb-4">
-                <h2 className="text-lg font-semibold">Google</h2>
+                <h2 className="text-lg font-semibold">{title}</h2>
                 <p className="text-sm text-gray-500">Software Engineering Jobs</p>
             </div>
 
-            {/* Button */}
             <button className="w-full flex items-center justify-center gap-2 bg-black text-white py-2 rounded-lg hover:bg-gray-800 transition">
                 View Jobs
                 <span>→</span>
--- src/components/HomeBody.jsx
+++ src/components/HomeBody.jsx
@@ -30,22 +30,13 @@ function HomeBody() {
 
 
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-20 p-6 ml-[3%]'>
-                <CompanyCard />
-                <CompanyCard />
-                <CompanyCard />
-                <CompanyCard />
-                <CompanyCard />
-                <CompanyCard />
-                
-
-
+                <CompanyCard companyLink="https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Search_logo.width-500.format-webp.webp" title="Google" />
+                <CompanyCard  companyLink="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSTWlnJCnn2ljYqv2t4XY9bM5U4pnohvXZRw&s" title="Microsoft"/>
+                <CompanyCard companyLink="https://cdn.freebiesupply.com/images/large/2x/amazon-logo-transparent.png" title="Amazon" />
+                <CompanyCard companyLink="https://download.logo.wine/logo/Apple_Inc./Apple_Inc.-Logo.wine.png" title="Apple" />
+                <CompanyCard companyLink="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGIT6YFPiNkWOhpi1KhzbKKGWAUn-v-OFr0Q&s" title="PayPal"/>
+                <CompanyCard companyLink="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Rakuten_Global_Brand_Logo.svg/1280px-Rakuten_Global_Brand_Logo.svg.png" title="Rakuten" />
             </div>
-
-
-
-
-
-
         </>
     )
 }
