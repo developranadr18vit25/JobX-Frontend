7214b16db6ad5c432113ed0e4ce8fd078a56e0df
developranadr18vit25 <ranadhruv10@gmail.com>
2026-06-17 18:47:16 +0530

    Recruiter to view the applicants

--- src/components/DisplayBody.jsx
+++ src/components/DisplayBody.jsx
@@ -20,7 +20,7 @@ function DisplayBody() {
 
       let res;
 
-      if(user.Roles=="Recruiter"){
+      if(role=="Applicant" || role==null){
         res = await api.getAvailableJobs("?" + params.toString());
       }
       else{
@@ -42,12 +42,25 @@ function DisplayBody() {
 
         {/* Header */}
         <div className="mb-6">
-          <h1 className="text-2xl font-semibold text-gray-900">
+          {
+            role=="Applicant"? <><h1 className="text-2xl font-semibold text-gray-900">
             Explore Jobs
-          </h1>
+          </h1> 
           <p className="text-sm text-gray-500 mt-1">
             Find roles matching your skills and experience
           </p>
+          </>
+          : 
+          <><h1 className="text-2xl font-semibold text-gray-900">
+            Job Listings
+          </h1>
+          <p className="text-sm text-gray-500 mt-1">
+            Manage and view all your job postings
+          </p>
+          </>
+          }
+          
+          
         </div>
 
         {/* Feed container */}
